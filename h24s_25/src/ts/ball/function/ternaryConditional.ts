6dae1c22fec447c4916644baf0351e6ab000a7b6
comavius <comavius@proton.me>
2024-06-15 23:07:15 +0900

    💡 order of partial application

--- h24s_25/src/ts/ball/function/ternaryConditional.ts
+++ h24s_25/src/ts/ball/function/ternaryConditional.ts
@@ -1,6 +1,9 @@
 import {FunctionBallInterface, type NumberBallInterface} from "@/ts/ballInterface";
 import {NumberBall} from "@/ts/numberball";
 
+/**
+ * カリー化された三項演算子?:のボール
+ */
 export class TernaryConditionalBall extends FunctionBallInterface<TernaryConditionalBall|null, ConditionalWithThenBall> {
     private funcVal: (x: NumberBallInterface) => {self: TernaryConditionalBall|null, other: ConditionalWithThenBall};
     
@@ -20,6 +23,9 @@ export class TernaryConditionalBall extends FunctionBallInterface<TernaryConditi
     }
 }
 
+/**
+ * TernaryConditionalに条件式が真の場合の出力を部分適用したボール
+ */
 export class ConditionalWithThenBall extends FunctionBallInterface<ConditionalWithThenBall|null, ConditionalWithBothBall> {
     private thenValue: number;
     private funcVal: (x: NumberBallInterface) => {self: ConditionalWithThenBall|null, other: ConditionalWithBothBall};
@@ -41,7 +47,9 @@ export class ConditionalWithThenBall extends FunctionBallInterface<ConditionalWi
     }
 }
 
-
+/**
+ * ConditionalWithThenBallに条件式が真の場合と偽の場合の出力を部分適用したボール
+ */
 export class ConditionalWithBothBall extends FunctionBallInterface<ConditionalWithBothBall|null, NumberBallInterface> {
     private thenValue: number;
     private elseValue: number;
