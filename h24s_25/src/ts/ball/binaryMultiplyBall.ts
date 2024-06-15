59605b5f2fe11cc43239836f7a58233325c3d09b
Mozuya <shibuya0507@kih.biglobe.ne.jp>
2024-06-15 17:03:46 +0900

    BinaryMultiplyBallの実装

---
+++ h24s_25/src/ts/ball/binaryMultiplyBall.ts
@@ -0,0 +1,16 @@
+import { FunctionBall } from '@/ts/ballfunction'
+
+export class BinaryMultiplyBall extends FunctionBall<(x: number) => number> {
+    constructor(position: { x: number, y: number }, velocity: { x: number, y: number }) {
+        super(
+            function (x: number) {
+                return function (y: number) {
+                    return x * y;
+                }
+            }
+            , "*", []
+        )
+        this.initialPosition = position;
+        this.initialVelocity = velocity;
+    }
+}
