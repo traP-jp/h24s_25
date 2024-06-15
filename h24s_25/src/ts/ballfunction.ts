5595424b47aa7b2b2cab600e8ad19ab2967a888e
shunapo <lingmushun25@gmail.com>
2024-06-15 15:10:06 +0900

    FunctionBallInterfaceに対応するクラスを作った(二回目

--- h24s_25/src/ts/ballfunction.ts
+++ h24s_25/src/ts/ballfunction.ts
@@ -1,14 +1,31 @@
 import{BallTypeEnum} from './balltypes';
-import {FunctionBallInterface} from './ballInterface';
+import type {FunctionBallInterface} from './ballInterface';
 class BallFunction<T> implements FunctionBallInterface<T>{
-      private x: number;
       symbol: string;
       applied: number[];
-      initialVelocity: { x: number, y: number };
-      initialPosition: { x: number, y: number };
+      initialVelocity: { x: number, y: number } = {x: 0, y: 0};
+      initialPosition: { x: number, y: number } = {x: 0, y: 0};
+      private funcVal: (x: number) => T;
 
-      ['constructor']: (func: (x: number) => T, symbol: string, applied: number[]) => void;
-      func: () => (x: number) => T;
-      ballType: () => BallTypeEnum.FUNCTION;
-      label: () => string;
+      constructor(func: (x: number) => T, symbol: string, applied: number[]) {
+            this.funcVal = func;
+            this.symbol = symbol;
+            this.applied = applied;
+      } 
+      func(): (x: number) => T {
+            return  this.funcVal;
+      }
+      ballType(): BallTypeEnum {
+            return BallTypeEnum.FUNCTION
+      }
+      label(): string{
+            let label: string = "";
+            this.applied.forEach(
+                  function (value) {
+                        label += `${value.toString} `
+                  }
+            )
+            label += this.symbol
+            return label
+      }
 }
